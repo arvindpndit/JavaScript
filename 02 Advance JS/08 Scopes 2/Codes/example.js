1542d5c4bb2dfc411c48cc3a2eed5e5c8f6d5e38
arvindpndit <arvindpandit9808@gmail.com>
2023-06-08 12:21:04 +0530

    scopes-hoising, autoglobals, undefined, undeclared

---
+++ 02 Advance JS/08 Scopes 2/Codes/example.js
@@ -0,0 +1,16 @@
+// var x = 20;
+// y = 30;
+// sum(x, y);
+// function sum(x, y) {
+//   console.log(x + y); //reference error
+// }
+
+var x = 20;
+y = 30;
+
+function sum(x, y) {
+  console.log(x + y);
+  50;
+}
+
+sum(x, y);
